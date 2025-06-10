3290b09b6d17dced958147fd959b04e32f6d87eb
Samantha Colon <samanthacolon@Mac.lan>
2025-06-09 20:39:04 -0400

    TAG-0-Day-3: Lists

---
+++ Class-1-Hello-World/Lists-intro.py
@@ -0,0 +1,26 @@
+list1 = [1,2,3,4,5]
+
+print(len(list1))
+
+print(list1)
+
+list2 = [22, "cheesecake bites", .5]
+
+print(list2)
+
+print(list2[0])
+
+print(list2[1])
+
+my_str = "abcdefg"
+
+print(my_str[1:2])
+
+#only prints out b because the first number is the start and the stop is the second number/value
+# will not print the stop variable 
+
+print(my_str[1:1])
+
+print(list2[0])
+
+print(list2[0:2])
